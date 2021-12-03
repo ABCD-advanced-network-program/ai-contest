3a835835e5b0c5a911a485340d0eaea74ae01c36
Lamisk <phuctrinh39@gmail.com>
2021-12-03 10:54:15 +0700

    add message error

--- ContestHolder/views.py
+++ ContestHolder/views.py
@@ -1,11 +1,13 @@
-from django.shortcuts import render,redirect
+from django.shortcuts import render, redirect
 from django.views import View
 from django import template
-from service import path,session
+from service import path, session
 from .data import mockData, mockUser, mockStatus
 from ContestAdmin import models
 from django.contrib.auth.models import User
 import os
+from django.contrib import messages
+
 
 class HolderView(View):
     def get(self, request):
@@ -14,7 +16,8 @@ class HolderView(View):
                 userName = request.user.username
             except:
                 userName = ''
-            obj = models.Contest.objects.filter(IDUser=request.user.id).order_by('-id')
+            obj = models.Contest.objects.filter(
+                IDUser=request.user.id).order_by('-id')
             # import datetime
             # now = datetime.datetime.now(obj[1].TimeStart.tzinfo)
             # if obj[1].TimeStart > now:
@@ -23,12 +26,13 @@ class HolderView(View):
                 'name': userName,
                 'dataContests': obj,
             }
-            return render(request, path.templateHolder , context)
+            return render(request, path.templateHolder, context)
         else:
             return redirect('login')
 
+
 class ContestDetail(View):
-    def get(self, request,id):
+    def get(self, request, id):
         if request.user.is_staff:
             userName = str()
             try:
@@ -37,10 +41,11 @@ class ContestDetail(View):
                 userName = ''
             detailData = models.Contest.objects.filter(id=id)
             data = []
-            obj = models.RegisterContest.objects.filter(IDContest=id).order_by('-id')
+            obj = models.RegisterContest.objects.filter(
+                IDContest=id).order_by('-id')
             for x in obj:
                 tg = {
-                    'name' : User.objects.get(id=x.IDUser).username
+                    'name': User.objects.get(id=x.IDUser).username
                 }
                 data.append(tg)
             context = {
@@ -51,7 +56,8 @@ class ContestDetail(View):
             return render(request, path.templateDetail, context)
         else:
             return redirect('login')
-    def post(self, request,id):
+
+    def post(self, request, id):
         try:
             content = request.FILES['content']
             kt_content = True
@@ -75,9 +81,12 @@ class ContestDetail(View):
         obj = models.Contest.objects.get(id=id)
         obj.Title = request.POST.get('title')
         obj.Description = request.POST.get('description')
-        obj.TimeRegister = request.POST.get('dateRegister')+' '+request.POST.get('timeRegister')
-        obj.TimeStart = request.POST.get('dateStart')+' '+request.POST.get('timeStart')
-        obj.TimeEnd = request.POST.get('dateEnd')+' '+request.POST.get('timeEnd')
+        obj.TimeRegister = request.POST.get(
+            'dateRegister')+' '+request.POST.get('timeRegister')
+        obj.TimeStart = request.POST.get(
+            'dateStart')+' '+request.POST.get('timeStart')
+        obj.TimeEnd = request.POST.get(
+            'dateEnd')+' '+request.POST.get('timeEnd')
         obj.TimeOut = request.POST.get('timeOut')
         obj.save()
         if kt_content == True:
@@ -88,23 +97,25 @@ class ContestDetail(View):
             with open(obj.LinkDataTrain, 'wb+') as destination:
                 for chunk in train.chunks():
                     destination.write(chunk)
-        if kt_test == True:           
+        if kt_test == True:
             with open(obj.LinkDataTest, 'wb+') as destination:
                 for chunk in test.chunks():
                     destination.write(chunk)
-        if kt_tester == True:  
+        if kt_tester == True:
             with open(obj.LinkTester, 'wb+') as destination:
                 for chunk in tester.chunks():
                     destination.write(chunk)
         return redirect('holder')
 
+
 class ContestDelete(View):
-    def get(self, request,id):
+    def get(self, request, id):
         print(id)
         obj = models.Contest.objects.get(id=id)
         obj.delete()
         return redirect('holder')
-        
+
+
 class CreateContest(View):
     def get(self, request):
         if request.user.is_staff:
@@ -119,6 +130,7 @@ class CreateContest(View):
             return render(request, path.templateCreate, context)
         else:
             return redirect('login')
+
     def post(self, request):
         try:
             content = request.FILES['content']
@@ -126,6 +138,8 @@ class CreateContest(View):
             test = request.FILES['test']
             tester = request.FILES['tester']
         except:
+            messages.error(
+                request, 'Create contest is not successful, please fill in full fields')
             return redirect('create')
         timeout = request.POST.get('timeOut')
         try:
@@ -133,13 +147,18 @@ class CreateContest(View):
             obj.IDUser = request.user.id
             obj.Title = request.POST.get('title')
             obj.Description = request.POST.get('description')
-            obj.TimeRegister = request.POST.get('dateRegister')+' '+request.POST.get('timeRegister')
-            obj.TimeStart = request.POST.get('dateStart')+' '+request.POST.get('timeStart')
-            obj.TimeEnd = request.POST.get('dateEnd')+' '+request.POST.get('timeEnd')
+            obj.TimeRegister = request.POST.get(
+                'dateRegister')+' '+request.POST.get('timeRegister')
+            obj.TimeStart = request.POST.get(
+                'dateStart')+' '+request.POST.get('timeStart')
+            obj.TimeEnd = request.POST.get(
+                'dateEnd')+' '+request.POST.get('timeEnd')
             obj.TimeOut = request.POST.get('timeOut')
             obj.save()
             id = str(obj.id)
         except:
+
+            messages.error(request, 'Create contest is not successful')
             return redirect('create')
         path = './static/contest/contest'+id
         if not os.path.exists(path):
@@ -164,9 +183,11 @@ class CreateContest(View):
         return redirect('holder')
 
 #############################PUBLIC###################
+
+
 class ContestStatus(View):
-    def get(self, request,id):
-        status = models.Status.objects.filter(IDcontest = id).order_by('-id')
+    def get(self, request, id):
+        status = models.Status.objects.filter(IDcontest=id).order_by('-id')
         data = []
         for x in status:
             language = "Error"
@@ -182,24 +203,24 @@ class ContestStatus(View):
                     for line in destination:
                         code = code + line
             except:
-                code+='File not found'
+                code += 'File not found'
             tg = {
-                'iduser' : x.id,
-                'name' : User.objects.get(id = x.IDUser).username,
-                'time' : str(x.TimeSubmit),
-                'language' : language,
-                'status' : x.Status,
-                'link' : x.LinkSubmit,
-                'code' :code
+                'iduser': x.id,
+                'name': User.objects.get(id=x.IDUser).username,
+                'time': str(x.TimeSubmit),
+                'language': language,
+                'status': x.Status,
+                'link': x.LinkSubmit,
+                'code': code
             }
             data.append(tg)
         # for x in data:
         #     print(x)
-        selectedContest = models.Contest.objects.filter(id = id)
+        selectedContest = models.Contest.objects.filter(id=id)
         context = {
             'name': request.user.username,
             'dataContests': selectedContest,
             'dataStatus': data
         }
         # print(context)
-        return render(request,path.templateStatus,context)
+        return render(request, path.templateStatus, context)
